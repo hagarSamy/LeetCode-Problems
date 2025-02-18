abfe7aec61e59390167646f1eb34edbfb33b9a8e
Hagar Samy <hs1800@fayoum.edu.eg>
2025-02-18 21:30:19 +0200

    utilised map structure

--- easyLevel/10-Two-Sum.js
+++ easyLevel/10-Two-Sum.js
@@ -14,11 +14,25 @@
  * @param {number} target
  * @return {number[]}
  */
+// var twoSum = function(nums, target) {
+//     for(let i = 0; i < nums.length; i++){
+//         for(let j = i + 1; j < nums.length; j++){
+//             if(nums[i] + nums[j] == target)
+//             return [i,j]
+//         }
+//     }
+// };
+
+// ##MORE EFFICIENT 😁⭐ runtime = 0 ms 🤞
 var twoSum = function(nums, target) {
-    for(let i = 0; i < nums.length; i++){
-        for(let j = i + 1; j < nums.length; j++){
-            if(nums[i] + nums[j] == target)
-            return [i,j]
+    let rest  = target;
+    let map = new Map();
+    for (let i = 0; i < nums.length; i++){
+        rest = target - nums[i];
+        if (map.has(rest)) {
+            return [map.get(rest), i]
         }
+        map.set(nums[i], i);
     }
+    return [];
 };
