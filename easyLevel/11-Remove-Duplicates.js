4c6708a259ba07cb304963a67aa4a73da0add2d1
Hagar Samy <hs1800@fayoum.edu.eg>
2025-02-21 22:08:44 +0200

    remove duplicates in place

---
+++ easyLevel/11-Remove-Duplicates.js
@@ -0,0 +1,32 @@
+// 26. Remove Duplicates from Sorted Array
+// Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
+
+// Consider the number of unique elements of nums to be k, to get accepted, you need to do the following things:
+
+// Change the array nums such that the first k elements of nums contain the unique elements in the order they were present in nums initially. The remaining elements of nums are not important as well as the size of nums.
+// Return k.
+
+// Example 1:
+
+// Input: nums = [1,1,2]
+// Output: 2, nums = [1,2,_]
+// Explanation: Your function should return k = 2, with the first two elements of nums being 1 and 2 respectively.
+// It does not matter what you leave beyond the returned k (hence they are underscores).
+
+////////////////Inefficient solution -- O(n) -- runtime = 60ms
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    let mapper = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        if (mapper.has(nums[i])) {
+            nums.splice(i, 1);
+            i--;
+        }
+        mapper.set(nums[i], i);
+    }
+    let k = nums.length;
+    return k;
+};
